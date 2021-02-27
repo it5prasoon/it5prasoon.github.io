5e9c5ca6bd2cda8bdf396873850ceb67a0d145e0
Prasoon <39785096+it5prasoon@users.noreply.github.com>
2021-02-27 10:42:17 +0530

    new portfolio added with javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,97 @@
 /*!
-    * Start Bootstrap - Agency v6.0.1 (https://startbootstrap.com/template-overviews/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
+    Title: Dev Portfolio Template
+    Version: 1.2.2
+    Last Change: 03/25/2020
+    Author: Ryan Fitzgerald
+    Repo: https://github.com/RyanFitzgerald/devportfolio-template
+    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 72,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
+    Description: This file contains all the scripts associated with the single-page
+    portfolio website.
+*/
+
+(function($) {
+
+    // Remove no-js class
+    $('html').removeClass('no-js');
+
+    // Animate to section when nav is clicked
+    $('header a').click(function(e) {
+
+        // Treat as normal link if no-scroll class
+        if ($(this).hasClass('no-scroll')) return;
+
+        e.preventDefault();
+        var heading = $(this).attr('href');
+        var scrollDistance = $(heading).offset().top;
+
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+
+        // Hide the menu once clicked if mobile
+        if ($('header').hasClass('active')) {
+            $('header, body').removeClass('active');
+        }
+    });
+
+    // Scroll to top
+    $('#to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // Scroll to first element
+    $('#lead-down span').click(function() {
+        var scrollDistance = $('#lead').next().offset().top;
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, 500);
+    });
+
+    // Create timeline
+    $('#experience-timeline').each(function() {
+
+        $this = $(this); // Store reference to this
+        $userContent = $this.children('div'); // user content
+
+        // Create each timeline block
+        $userContent.each(function() {
+            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        });
+
+        // Add icons to each block
+        $this.find('.vtimeline-point').each(function() {
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        });
+
+        // Add dates to the timeline if exists
+        $this.find('.vtimeline-content').each(function() {
+            var date = $(this).data('date');
+            if (date) { // Prepend if exists
+                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
             }
-        }
+        });
+
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
+    // Open mobile menu
+    $('#mobile-menu-open').click(function() {
+        $('header, body').addClass('active');
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 74,
+    // Close mobile menu
+    $('#mobile-menu-close').click(function() {
+        $('header, body').removeClass('active');
     });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+    // Load additional projects
+    $('#view-more-projects').click(function(e){
+        e.preventDefault();
+        $(this).fadeOut(300, function() {
+            $('#more-projects').fadeIn(300);
+        });
+    });
+
+})(jQuery);
