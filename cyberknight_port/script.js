c10b04aabf23271659561d4ba9a03e0fc7ffcbf0
Prasoon Kumar <prasoonk187@gmail.com>
2024-08-31 10:57:07 +0530

    added new experience and css changes

--- cyberknight_port/script.js
+++ cyberknight_port/script.js
@@ -20,21 +20,36 @@ for (var i=0; themeDots.length > i; i++){
 }
 
 function setTheme(mode){
-	if(mode == 'light'){
+	if(mode === 'light'){
 		document.getElementById('theme-style').href = 'default.css'
 	}
 
-	if(mode == 'blue'){
+	if(mode === 'blue'){
 		document.getElementById('theme-style').href = 'blue.css'
 	}
 
-	if(mode == 'green'){
+	if(mode === 'green'){
 		document.getElementById('theme-style').href = 'green.css'
 	}
 
-	if(mode == 'purple'){
+	if(mode === 'purple'){
 		document.getElementById('theme-style').href = 'purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
+}
+
+function toggleItems() {
+	const moreItems = document.getElementById("more-items");
+	const toggleButton = document.getElementById("toggleButton");
+
+	if (moreItems.style.display === "none") {
+		moreItems.style.display = "block";
+		toggleButton.textContent = "Less"; // Change button text to "Less"
+	} else {
+		moreItems.style.display = "none";
+		toggleButton.textContent = "More"; // Change button text back to "More"
+		// Scroll to the top of the list or a specific element when collapsing
+		document.querySelector('.list-component').scrollIntoView({ behavior: 'instant' });
+	}
 }
